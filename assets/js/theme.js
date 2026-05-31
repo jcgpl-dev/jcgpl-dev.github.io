d16bbaab07d66b1b0e0c4d348d010e740001d235
Jesie Gapol <gapoljesie23@gmail.com>
2026-05-31 23:54:44 +0800

    added auto detet system theme

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -16,12 +16,12 @@ function updateHeatmap() {
 }
 
 // 1. Check local storage or system preference on initial load
-// const savedTheme = localStorage.getItem('theme');
-// const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+const savedTheme = localStorage.getItem('theme');
+const systemPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-// if (savedTheme === 'dark' || (!savedTheme && systemPrefersDark)) {
-//   body.classList.add('dark-mode');
-// }
+if (savedTheme === 'dark' || (!savedTheme && systemPrefersDark)) {
+  body.classList.add('dark-mode');
+}
 
 
 updateHeatmap();
